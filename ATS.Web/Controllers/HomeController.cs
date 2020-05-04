81388c81f8d891aec3f65dbf079816fdd5fe7e9c
Sakchai Thanasamut <sakchai.t@qnet.net>
2020-05-04 23:11:21 +0800

    initial version

---
+++ ATS.Web/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ATS.Web.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace ATS.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index(PersonAccessViewModel persons)
+        {
+            if (persons.TotalValue == null)
+            {
+                persons.Failed = 147;
+                persons.Passed = 1607;
+                persons.FailedValue = "147";
+                persons.PassedValue = "1,607";
+                persons.TotalValue = "1,754";
+                persons.PercentPass = "91.6";
+                persons.StartTime = DateTime.Today;
+                persons.EndTime = DateTime.Now;
+                persons.BuildingId = 1;
+                var lists = new List<SelectListItem>();
+                lists.Add(new SelectListItem { Selected = true, Text = "Building A", Value = "1" });
+                lists.Add(new SelectListItem { Selected = false, Text = "Building B", Value = "2" });
+                persons.Buildings = lists;
+            } 
+            return View(persons);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ATS.Web/Models/PersonAccessViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ATS.Web.Models
+{
+    public class PersonAccessViewModel
+    {
+        public string TotalValue { get; set; }
+        public string PassedValue { get; set; }
+        public string FailedValue { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public int Passed { get; set; }
+        public int Failed { get; set; }
+        public string PercentPass { get; set; }
+        public int BuildingId { get; set; }
+        public List<SelectListItem> Buildings { get; set; }
+    }
+}
